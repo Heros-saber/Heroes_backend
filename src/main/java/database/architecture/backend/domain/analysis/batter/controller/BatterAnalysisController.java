d3bfb38ac5fbda1a093e43c6a20b5b9fa4a37887
Johyeonwoooooooo <hyunwoo02031@naver.com>
2024-11-25 17:55:44 +0900

    feat: 엔드포인트 수정

--- src/main/java/database/architecture/backend/domain/analysis/batter/controller/BatterAnalysisController.java
+++ src/main/java/database/architecture/backend/domain/analysis/batter/controller/BatterAnalysisController.java
@@ -5,14 +5,17 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/batter/analysis")
 public class BatterAnalysisController {
     private final BatterAnalysisService service;
-    @GetMapping("/test")
-    public ResponseEntity<?> test(String name){
+    @GetMapping("/{name}")
+    public ResponseEntity<?> test(@PathVariable String name){
 
         return ResponseEntity.ok(service.one_line_analysis(name));
     }
